4e56ea94cb7fc7c8ee49f7ab34913a93159dbbde
Evan Ng <75152390+evan-ng@users.noreply.github.com>
2022-08-17 16:11:51 -0400

    Add colour representation classes and functions to convert between them

---
+++ script.js
@@ -0,0 +1,220 @@
+"use strict";
+
+/*
+ * Classes for RGB, CYMK, HSV, and HSL colour representations
+ */
+class RGB {
+  constructor(r, g, b) {
+    this.r = r;
+    this.g = g;
+    this.b = b;
+  }
+}
+
+class CYMK {
+  constructor(c, y, m, k) {
+    this.c = c;
+    this.y = y;
+    this.m = m;
+    this.k = k;
+  }
+}
+
+class HSL {
+  constructor(h, s, l) {
+    this.h = h;
+    this.s = s;
+    this.l = l;
+  }
+}
+
+class HSV {
+  constructor(h, s, v) {
+    this.h = h;
+    this.s = s;
+    this.v = v;
+  }
+}
+
+/*
+ * Colour conversion functions
+ */
+RGB.prototype.toCYMK = function() {
+  let rd = this.r / 255;
+  let gd = this.g / 255;
+  let bd = this.b / 255;
+  let k = 1 - Math.max(rd, gd, bd);
+  let rk = 1 - k;
+  let c = (rk - rd) / rk;
+  let y = (rk - bd) / rk;
+  let m = (rk - gd) / rk;
+  c = Math.round(c * 100);
+  y = Math.round(y * 100);
+  m = Math.round(m * 100);
+  k = Math.round(k * 100);
+  return new CYMK(c, y, m, k);
+}
+
+RGB.prototype.toHSL = function() {
+  let rd = this.r/255;
+  let gd = this.g/255;
+  let bd = this.b/255;
+  let max = Math.max(rd, gd, bd);
+  let min = Math.min(rd, gd, bd);
+  let c = max - min;
+
+  let h;
+  if (c == 0) {
+    h = 0;
+  } else {
+    let segment;
+    let shift;
+    switch (max) {
+      case rd:
+        segment = (gd - bd) / c;
+        shift = (segment < 0) ? 6 : 0;
+        break;
+      case gd:
+        segment = (bd - rd) / c;
+        shift = 2;
+        break;
+      case bd:
+        segment = (rd - gd) / c;
+        shift = 4;
+        break;
+    }
+    h = (segment + shift) * 60;
+    h = Math.round(h);
+  }
+
+  let l = (max + min) / 2;
+  let s = (c == 0) ? 0 : (c / (1 - Math.abs(2 * l - 1)));
+
+  s = Math.round(s * 100);
+  l = Math.round(l * 100);
+
+  return new HSL(h, s, l);
+}
+
+RGB.prototype.toHSV = function() {
+  let rd = this.r/255;
+  let gd = this.g/255;
+  let bd = this.b/255;
+  let max = Math.max(rd, gd, bd);
+  let min = Math.min(rd, gd, bd);
+  let c = max - min;
+
+  let h;
+  if (c == 0) {
+    h = 0;
+  } else {
+    let segment;
+    let shift;
+    switch (max) {
+      case rd:
+        segment = (gd - bd) / c;
+        shift = (segment < 0) ? 6 : 0;
+        break;
+      case gd:
+        segment = (bd - rd) / c;
+        shift = 2;
+        break;
+      case bd:
+        segment = (rd - gd) / c;
+        shift = 4;
+        break;
+    }
+    h = (segment + shift) * 60;
+    h = Math.round(h);
+  }
+
+  let s = (max == 0) ? 0 : Math.round(100 * c/max);
+  let v = Math.round(max * 100);
+
+  return new HSV(h, s, v);
+}
+
+CYMK.prototype.toRGB = function() {
+  let rk = 1 - this.k;
+  let r = 255 * (1 - this.c) * rk;
+  let g = 255 * (1 - this.c) * rk;
+  let b = 255 * (1 - this.c) * rk;
+
+  return new RGB(r, g, b);
+}
+
+CYMK.prototype.toHSL = function() {
+  let rgb = this.toRGB();
+  return rgb.toHSL();
+}
+
+CYMK.prototype.toHSV = function() {
+  let rgb = this.toRGB();
+  return rgb.toHSV();
+}
+
+HSL.prototype.toRGB = function() {
+  let c = (1 - Math.abs(2 * this.l)) * this.s;
+  let x = (1 - Math.abs(((H / 60) % 2) - 1)) * c;
+  let m = this.l - c/2;
+  let r, g, b;
+  if (h < 60) { r = c; g = x; b = 0; }
+  else if (h < 120) { r = x; g = c; b = 0; }
+  else if (h < 180) { r = 0; g = c; b = x; }
+  else if (h < 240) { r = 0; g = x; b = c; }
+  else if (h < 300) { r = x; g = 0; b = c; }
+  else { r = c; g = 0; b = x; }
+  r = (r + m) * 255;
+  g = (g + m) * 255;
+  b = (b + m) * 255;
+  return new RGB(r, g, b);
+}
+
+HSL.prototype.toCYMK = function() {
+  let rgb = this.toRGB();
+  return rgb.toCYMK();
+}
+
+HSL.prototype.toHSV = function() {
+  let h = this.h;
+  let s = this.s / 100;
+  let l = this.l / 100;
+  let v = l + s * Math.min(l, 1 - l);
+  s = (v == 0) ? 0 : (2 * (1 - l/v));
+  s = Math.round(s * 100);
+  v = Math.round(v * 100);
+  return new HSV(h, s, v);
+}
+
+HSV.prototype.toRGB = function() {
+  let c = (this.v / 100) * (this.s / 100);
+  let x = (1 - Math.abs(((H / 60) % 2) - 1)) * c;
+  let m = (this.v / 100) - c;
+  let r, g, b;
+  if (h < 60) { r = c; g = x; b = 0; }
+  else if (h < 120) { r = x; g = c; b = 0; }
+  else if (h < 180) { r = 0; g = c; b = x; }
+  else if (h < 240) { r = 0; g = x; b = c; }
+  else if (h < 300) { r = x; g = 0; b = c; }
+  else { r = c; g = 0; b = x; }
+  r = (r + m) * 255;
+  g = (g + m) * 255;
+  b = (b + m) * 255;
+  return new RGB(r, g, b);
+}
+
+HSV.prototype.toCYMK = function() {
+  let rgb = this.toRGB();
+  return rgb.toCYMK();
+}
+
+HSV.prototype.toHSL = function() {
+  let h = this.h;
+  let s = this.s / 100;
+  let v = this.v / 100;
+  let l = v * (1 - s/2);
+  s = (l == 0 || l == 1) ? 0 : ((v - l) / Math.min(l, 1 - l));
+  s = Math.round(s * 100);
+  l = Math.round(l * 100);
+  return new HSL(h, s, l);
+}
